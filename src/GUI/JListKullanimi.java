78ca63b3be972ee3e19b31aeb181b633f06ccfc2
Doğukan ARICAN <dogukanarican@outlook.com>
2023-03-21 03:14:33 +0300

    Initial commit

--- src/GUI/JListKullanimi.java
+++ src/GUI/JListKullanimi.java
@@ -12,10 +12,10 @@ public class JListKullanimi {
         JLabel lbl1,lbl2;
 
         lbl1 = new JLabel();
-        lbl1.setBounds(50,100,100,100);
+        lbl1.setBounds(150,30,100,100);
 
         lbl2 = new JLabel();
-        lbl2.setBounds(100,100,75,75);
+        lbl2.setBounds(150,200,100,100);
 
         DefaultListModel<String > l1 = new DefaultListModel<>();
         l1.addElement("Kuzey");
@@ -24,7 +24,7 @@ public class JListKullanimi {
         l1.addElement("Doğu");
 
         JList<String> liste1 = new JList<>(l1);
-        liste1.setBounds(100,100,75,75);
+        liste1.setBounds(30,30,100,150);
 
         DefaultListModel<String> l2 = new DefaultListModel<>();
         l2.addElement("North");
@@ -33,29 +33,33 @@ public class JListKullanimi {
         l2.addElement("East");
 
         JList<String> liste2 = new JList<>(l2);
-        liste2.setBounds(100,200,75,75);
+        liste2.setBounds(30,200,100,150);
 
         btn1 = new JButton("Seçimi Göster");
-        btn1.setBounds(100,200,75,75);
+        btn1.setBounds(150,30,125,30);
         btn1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                lbl1.setText("");
+                lbl1.setText("lbl1");
             }
         });
 
         btn2 = new JButton("Show Selected");
-        btn2.setBounds(100,200,75,75);
+        btn2.setBounds(150,200,125,30);
         btn2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                lbl2.setText("");
+                lbl2.setText("lbl2");
             }
         });
 
 
         f.add(liste1);
         f.add(liste2);
+        f.add(lbl1);
+        f.add(lbl2);
+        f.add(btn1);
+        f.add(btn2);
         f.setSize (800, 600);
         f.setLayout (null);
         f.setVisible(true);
